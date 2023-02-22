4e09a3873f3fdc801a44894892de14fed11690d3
Nemanja2912 <nemanja.mijailovic2912@gmail.com>
2023-02-22 05:50:12 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Navigation from "./components/navigation/navigation";
 
 import News from "./components/news/news";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/home";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/news" element={<News />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
